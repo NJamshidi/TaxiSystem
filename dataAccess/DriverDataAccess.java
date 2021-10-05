7729f767ccbf4e7f542f888c3280b7cb80e1b095
Narges Jamshidi <90821930+NJamshidi@users.noreply.github.com>
2021-10-05 15:47:26 +0330

    Complete feature for Driver

--- dataAccess/DriverDataAccess.java
+++ dataAccess/DriverDataAccess.java
@@ -1,6 +1,7 @@
 package taxiSystem.dataAccess;
 
-import taxiSystem.model.Driver;
+import taxiSystem.enumeration.TypeOfVehicle;
+import taxiSystem.model.person.Driver;
 
 import java.sql.*;
 import java.util.ArrayList;
@@ -11,11 +12,16 @@ public class DriverDataAccess {
     public int addDriver(Driver driver) throws SQLException {
         Connection connection = SqlConnection.getConnection();
         if (connection != null) {
-            PreparedStatement preparedStatement = connection.prepareStatement("insert into drivers values (?, ?, ?, ?)");
+            PreparedStatement preparedStatement = connection.prepareStatement("insert into drivers values (?, ?, ?, ?,?,?,?,?,?)");
             preparedStatement.setString(1, driver.getDriverUserName());
             preparedStatement.setString(2, driver.getName());
             preparedStatement.setString(3, driver.getFamily());
             preparedStatement.setString(4, driver.getNationalCode());
+            preparedStatement.setString(5, driver.getPhoneNumber());
+            preparedStatement.setInt(6, driver.getAge());
+            preparedStatement.setBoolean(7, driver.getTripStatus());
+            preparedStatement.setInt(8, driver.getVehicleId());
+            preparedStatement.setString(9, TypeOfVehicle.CAR.getAbbr());
 
             int i = preparedStatement.executeUpdate();
             return i;
@@ -38,6 +44,11 @@ public class DriverDataAccess {
                 driver.setName(resultSet.getString("name"));
                 driver.setFamily(resultSet.getString("family"));
                 driver.setNationalCode(resultSet.getString("nationalCode"));
+                driver.setPhoneNumber(resultSet.getString("phoneNumber"));
+                driver.setAge(resultSet.getInt("age"));
+                driver.setTripStatus(resultSet.getBoolean("tripStatus"));
+                driver.setVehicleId(resultSet.getInt("vehicleId"));
+                driver.setTypeOfVehicle(TypeOfVehicle.CAR);
 
                 drivers.add(driver);
             }
@@ -62,6 +73,11 @@ public class DriverDataAccess {
                 driv.setName(rs.getString("name"));
                 driv.setFamily(rs.getString("family"));
                 driv.setNationalCode(rs.getString("nationalCode"));
+                driv.setPhoneNumber(rs.getString("phoneNumber"));
+                driv.setAge(rs.getInt("age"));
+                driv.setTripStatus(rs.getBoolean("tripStatus"));
+                driv.setVehicleId(rs.getInt("vehicleId"));
+                driv.setTypeOfVehicle(TypeOfVehicle.CAR);
             }
             return driv.toString();
         } else {
